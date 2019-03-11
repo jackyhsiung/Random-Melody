834088ebb7911b3d2393aa1591f315461f4f9ebc
Jacky Hsiung <forlol1123@gmail.com>
2019-03-11 23:28:41 +0900

    add random melody for 8 bars and refactor

--- rm.py
+++ rm.py
@@ -1,24 +1,34 @@
 from pyknon.genmidi import Midi
 from pyknon.music import NoteSeq, Note
-#a = NoteSeq("C3 D5 E5 F5")
+import random
 
-root = input('Enter the root of the chord: ')
-chord = input('What kind of chord:(Maj7/m7) ')
-# Set Interval Relationships in Appregio Notes 
-half_steps = []
-notes_5th = []
-if chord == 'Maj7':
-	half_steps = [0, 4, 7 ,11]
-	notes_5th = [7, 7, 7, 6]
-elif chord == 'm7':
-	half_steps = [0, 3, 7, 10]
-	notes_5th = [7, 7, 7, 7]
-sequence = []
-for a, b in zip(half_steps, notes_5th):
-	sequence.append(Note(a))
-	sequence.append(Note(a).transposition(b))
+def random_melody(note_list, octave_low, octave_high, dur_list):
+	rm = NoteSeq() #[]
+	melody_len = 0
+	while melody_len < 8: # while True if break bug
+		pitch = random.choice(note_list) #refactor by root +2 +4 ...
+		octave = random.randint(octave_low, octave_high)
+		dur = random.choice(dur_list)
+		vol = random.randrange(80, 125, 5) #refactor
+		print([pitch, octave, dur, vol])
+		rm.append(Note(pitch, octave, dur, vol))
+		melody_len += dur
+	print(melody_len)
+	print(rm)
+	return rm
 
-notes3 = NoteSeq(sequence)
-midi = Midi(number_tracks=1, tempo=90)
-midi.seq_notes(notes3, track=0)
-midi.write("demo3.mid")
+def gen_midi(rm, filename):
+	midi = Midi(number_tracks=1, tempo=120)
+	midi.seq_notes(rm, track=0) # no more than 0
+	midi.write(filename)
+
+def main():
+	note_list = [0, 2, 4, 5, 7, 9, 11] #refactor by root +2 +4 ...
+	octave_low = 5
+	octave_high = 6
+	dur_list = [0.125, 0.25, 0.5]
+	rm = random_melody(note_list, octave_low, octave_high, dur_list)
+	print(rm)
+	gen_midi(rm, 'random_melody_in_C.mid')
+
+main()
